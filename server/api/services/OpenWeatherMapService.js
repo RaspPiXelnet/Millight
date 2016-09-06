8a252c1cec55429000fe4638bc283292630d837f
mnicole <mnicole@stepnet.fr>
2016-09-06 22:54:43 +0200

    Update OpenWeatherMap Service

--- server/api/services/OpenWeatherMapService.js
+++ server/api/services/OpenWeatherMapService.js
@@ -6,10 +6,38 @@ var OpenWeatherMap = {
   city: sails.config.rpiApp.openWeatherMap.city,
   unit: sails.config.rpiApp.openWeatherMap.unit,
 
-  getInfo: function (cb) {
+  getCurrent: function (cb) {
     request.get(
       {
         url: 'http://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&units=' + this.unit + '&APPID=' + this.apikey,
+      }, function (err, httpResponse, body) {
+        if (err) {
+          cb({err: err, httpResponse: httpResponse, data: false});
+        } else {
+          var response = JSON.parse(body);
+          cb({err: false, httpResponse: httpResponse, data: response});
+        }
+      });
+  },
+
+  getRecentForecast: function (cb) {
+    request.get(
+      {
+        url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + this.city + '&units=' + this.unit + '&APPID=' + this.apikey,
+      }, function (err, httpResponse, body) {
+        if (err) {
+          cb({err: err, httpResponse: httpResponse, data: false});
+        } else {
+          var response = JSON.parse(body);
+          cb({err: false, httpResponse: httpResponse, data: response});
+        }
+      });
+  },
+
+  getAdvancedForecast: function (cb) {
+    request.get(
+      {
+        url: 'api.openweathermap.org/data/2.5/forecast/daily?q=' + this.city + '&cnt=16&units=' + this.unit + '&APPID=' + this.apikey,
       }, function (err, httpResponse, body) {
         if (err) {
           cb({err: err, httpResponse: httpResponse, data: false});
